a921228a0deedc1cbd14da2f9d51a890248e0d93
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2015-02-23 17:22:31 +0100

    Icons theme & fonts integration

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ docker run \
 	-e DISPLAY \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /run/user/$UID/pulse:/run/pulse \
+	\
 	-v $HOME:/home/dev \
+	-v /usr/share/fonts:/usr/local/share/fonts:ro \
+	-v /usr/share/themes:/usr/local/share/themes:ro \
+	-v /usr/share/icons:/usr/local/share/icons:ro \
 	\
 	gnome-builder
